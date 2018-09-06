7a776e6638538ce6e1936afe418783fa8bcfa979
Zac Willmington <zac.willmington@gmail.com>
2018-09-06 01:00:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+
+customerName = "bob";
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer = "primm";
+    leastFavoriteCustomer = "Nadia";
+}
